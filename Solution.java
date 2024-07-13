a57bc3c1a0d6da3249ea8a09b7aa077a65bcf514
GamGomYang <146828148+GamGomYang@users.noreply.github.com>
2024-07-13 20:39:04 +0900

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public boolean isPalindrome(String s){
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }
+            else if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
+            else if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Solution solution = new Solution();
+        boolean result = solution.isPalindrome(input);
+        System.out.println(result);
+    }
+}
