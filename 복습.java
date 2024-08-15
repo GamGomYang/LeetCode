5b113795409915e4972de803480ec63021496155
GamGomYang <ay6656@naver.com>
2024-08-15 19:43:13 +0900

    miss solution

--- 복습.java
+++ 복습.java
@@ -1,69 +1,60 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+
+public class 복습{
 
 
-public class Solution{
-    public List<List<Integer>> fourSum( int[] nums , int target){
-
-        List<List<Integer>> result = new ArrayList<>();
-        //list<list> == arrayList;
+    public int lengthOfLastWord(String s){
 
 
-        if(nums.length <4){
-            return result;
-        }
+        String[] words = s.split(" ");
 
-        int n = nums.length;
-
-        for(int i =0 ; i < n-3 ; i++ ){
-            if(nums[i]== nums[i-1]&& i>0){
-                continue;
-            }
-            for(int j = i+1 ; j< n-2; j++){
-                if(j>i+1 && nums[j]==nums[j-1]){
-
-                    continue;
-                }
+        int maxlength =0;
 
 
-                int left = j+1;
-                int right = n-1;
+        for(String word: words){
 
-                while(left<right){
-                    long sum = (long) nums[i]+ nums[j]+ nums[left]+ nums[right];
+            if(!word.isEmpty()){
 
-                    if(sum == target){
-                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-
-                    
-                    while(left<right && nums[left]==nums[left+1]){
-                        left++;
-                    }
-                    while(left<right&& nums[right]==nums[right-1]){
-                        right--;
-                    }
-
-                    left++;
-                    right--;}
-
-                    else (sum<target){
-                        left++;
+                maxlength = Math.max(maxlength, word.length());
 
 
-                    }
-             
-                    }
-                    
-                }
+
+
+
+
             }
 
-            
+
         }
 
-
-
-    
+        return maxlength;
     }
+
+
+    public static void main(String[] args) {
+        
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        String s = scanner.nextLine();
+
+
+        복습 solution = new 복습();
+
+        int result = solution.lengthOfLastWord(s);
+
+
+        System.out.println(result);
+
+
+
+
+
+    }
+
+
+
+
 }
