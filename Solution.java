213dcd48ca2eb268204a0753e45565f69b3d59c8
GamGomYang <ay6656@naver.com>
2024-08-18 17:37:23 +0900

    0815 modified

--- Solution.java
+++ Solution.java
@@ -1,54 +1,51 @@
 import java.util.List;
+import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.LinkedList;
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode root = new ListNode(0);
-        root.next = head;
 
-        if(head == null || head.next == null){
-            return head;
+
+public class Solution{
+
+
+   public List<List<Integer>> generate(int numRows){
+
+
+
+
+    List<List<Integer>> result =  new ArrayList();
+
+    int n = numRows;
+
+
+    for(int i=0; i< n;i++){
+
+
+
+
+        List<Integer> row = new ArrayList();
+
+        row.add(1);
+
+
+        for(int j=1; j<i;j++){
+
+            List<Integer> prerow =  result.get(i-1);
+
+
+            int value = prerow.get(j-1)+prerow.get(j);
+
+            row.add(value);
         }
 
-        ListNode start = root.next;
-        ListNode end = start.next;
-        ListNode current = head;
-        int count =0;
-
-        while(current != null){
-            count++;
-            current = current.next;
-
-
+        if(i>0){
+            row.add(1);
         }
 
 
-        for(int i=1; i<count; i++ ){
-            ListNode temp = start.next;
-            start.next = end.next;
-            end.next = root.next;
-            root.next = end;
-            end = start.next;
-            
-
-
-        }
-        
-
-        return root.next;
-        
-
-
-
+        result.add(row);
     }
+
+
+
+    return result;
+   }
 }
