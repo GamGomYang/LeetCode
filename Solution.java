6b7b1faf25136880bcdadfb898e78e2ed27a659e
GamGomYang <ay6656@naver.com>
2024-09-29 09:27:37 +0900

    Solution modified

--- Solution.java
+++ Solution.java
@@ -1,37 +1,44 @@
 import java.util.Scanner;
 import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.io.*;
 
 
-//leetcode 9번 문제 확인
-
-
 public class Solution{
+
 
     public boolean isPalindrome(int x){
 
         if(x<0){
             return false;
         }
-
-        //정수 배열 처리 -> 숫자를 문자열 처리하여서 배열로 처맄
         String string = String.valueOf(x);
+        int length = string.length();
+
+        int right = length;
         int left =0;
-        int right = string.length()-1;
 
-        while(right>left){
+        while(left<right){
 
-            if(string.charAt(left)!= string.charAt(right)){
-                return false;
+            if(string.charAt(right)== string.charAt(left)){
+
+                right--;
+                left++;
             }
 
-            left++;
-            right--;
+            else{
+                return false;
+            }
+            return true;
         }
+    }
 
-        return true;
+    public static void main(String[] args) {
         
+      Scanner scanner =  new Scanner(System.in);
+      int result = scanner.nextInt();
+      Solution solution = new Solution();
 
-    }}
+      solution.isPalindrome(result);
+    }
+}
