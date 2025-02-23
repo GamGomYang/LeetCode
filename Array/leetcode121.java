fe5b26398850cf1514b63b359767202a3f9cdc67
GamGomYang <ay6656@naver.com>
2025-02-23 17:28:24 +0900

    02.23.01

--- Array/leetcode121.java
+++ Array/leetcode121.java
@@ -1,3 +1,4 @@
+package Array;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.math.*;
--- Solution.java
+++ Solution.java
@@ -1,57 +1,28 @@
-<<<<<<< HEAD
-import java.math.BigInteger;
+import java.util.*;
 
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // 두 리스트를 뒤집음
-        ListNode l1Reversed = reverseList(l1);
-        ListNode l2Reversed = reverseList(l2);
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0, n = intervals.length;
 
-        // 두 리스트를 정수로 변환 후 덧셈
-        BigInteger result = toBigInt(l1Reversed).add(toBigInt(l2Reversed));
-
-        // 결과를 뒤집힌 리스트로 변환
-        return toReversedLinkedList(result);
-    }
-
-    // 리스트 뒤집기
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null, node = head;
-
-        while (node != null) {
-            ListNode next = node.next; // 다음 노드 저장
-            node.next = prev;         // 현재 노드가 이전 노드를 가리킴
-            prev = node;              // 이전 노드 업데이트
-            node = next;              // 다음 노드로 이동
-        }
-        return prev; // 뒤집힌 리스트의 첫 노드
-    }
-
-    // 리스트를 BigInteger로 변환
-    public BigInteger toBigInt(ListNode node) {
-        StringBuilder val = new StringBuilder();
-
-        while (node != null) {
-            val.append(node.val); // 각 노드 값을 문자열로 연결
-            node = node.next;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
         }
 
-        return new BigInteger(val.toString()); // 문자열을 정수로 변환
-    }
-
-    // BigInteger를 뒤집힌 리스트로 변환
-    public ListNode toReversedLinkedList(BigInteger val) {
-        ListNode prev = null;
-
-        for (char c : val.toString().toCharArray()) {
-            ListNode node = new ListNode(Character.getNumericValue(c)); // 각 문자 -> 숫자로 변환
-            node.next = prev; // 현재 노드가 이전 노드를 가리킴
-            prev = node;      // 이전 노드 업데이트
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
+        result.add(newInterval);
+        
 
-        return prev; // 리스트의 첫 노드 반환
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+        
+        return result.toArray(new int[result.size()][]);
     }
 }
-=======
-
->>>>>>> d55ec8d9378a5c5320247cc991720984802a45b2
