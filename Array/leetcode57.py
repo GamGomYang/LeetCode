e4e29c0d218fcef3e6e02fe5c41d07dd1978c091
GamGomYang <ay6656@naver.com>
2025-03-07 12:37:35 +0900

    03.07.02

--- Array/leetcode57.py
+++ Array/leetcode57.py
@@ -1,19 +1,27 @@
 from typing import List
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        inserted = False  
+    def insert(self, intervals: List[List[int]],newInterval: List[int])-> List[List[int]]:
+
+        res =[]
+
+
 
         for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
+        # 1. newInterval 이 intervals의 범위에 속하지 않는 경우 
+            
+            # 1.1 newInterval 이 intervals보다 작은 경우
+            if newInterval[1]<intervals[i][0]:
                 res.append(newInterval)
-                return res + intervals[i:]  
+                return res+intervals[i:]
 
-            elif newInterval[0] > intervals[i][1]:  
+            # 1.2 newInterval이 intervals보다 큰 경우
+            elif newInterval[0]>intervals[i][1]:
                 res.append(intervals[i])
-            else:  
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
 
-        res.append(newInterval)  
+            # 2. newInterval과 intervals의 구역이 겹치는 경우
+            else:
+                newInterval = [min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
+            
+        res.append(newInterval)
         return res
