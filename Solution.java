446be06dc9939dd3d597f920c37b181374596689
GamGomYang <146828148+GamGomYang@users.noreply.github.com>
2025-04-04 15:44:33 +0900

    04.04.01 LTC05 modified

--- Solution.java
+++ Solution.java
@@ -1,34 +1,53 @@
+import com.sun.tools.javac.Main;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.uitl.List;
 import java.util.Arrays;
-import java.util.List;
+
 
 class Solution {
-   public List<List<Integer>> combinationSum(int[] candidates, int target) {
-   //       List<List<Integer>> result = new ArrayList<>();
-   //       Arrays.sort(candidates); 
-   //       backtrack(candidates, target, 0, new ArrayList<>(), result);
-   //       return result;
-   //   }
- 
-   //   private void backtrack(int[] candidates, int target, int startIndex, 
-   //                          List<Integer> current, List<List<Integer>> result) {
-   //       if (target == 0) {
-   //           result.add(new ArrayList<>(current)); 
-   //           return;
-   //       }
- 
-   //       for (int i = startIndex; i < candidates.length; i++) {
-   //           int pick = candidates[i];
- 
-   //           if (pick > target) {
-   //               break;
-   //           }
- 
-   //           current.add(pick);                      
-   //           backtrack(candidates, target - pick, i, current, result); 
-   //           current.remove(current.size() - 1);    
-   //       }
-   //    }
-     
-   }
+    public String longestPalindrome(String s) {
+
+        int length = s.length();
+
+        StringBuilder sb  = new StringBuilder();
+
+        int left =0;
+        int right = length;
+
+        char[] chars = new char[length];
+        chars = s.toCharArray();
+
+        for(int i=0 ; i<length-1; i++){
+
+            if(chars[left] == chars[right]){
+                right--;
+                left++;
+            }
+            else{
+                right--;
+            }
+        
+        }
+    }
+
+    public static void main(String[] args){
+
+    Solution solution = new Solution();
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println(" input ");
+    
+    String input = scanner.nextLine();
+
+    String result;
+
+    result = solution.longestPalindrome(input);
+
+    System.out.println(result);
+
+
+
+    
+    }
+}
