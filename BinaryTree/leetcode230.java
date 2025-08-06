348a92b6893acea90b0f0a7132d55fdb93b80cec
GamGomYang <ay6656@naver.com>
2025-08-06 15:45:46 +0900

    08.06 LTC230 solved

---
+++ BinaryTree/leetcode230.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class leetcode230 {
+    
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int counter = 0;
+    int answer = -1;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inOrder(root,k);
+        return answer;
+    }
+
+    public void inOrder(TreeNode node, int k){
+
+        if(node == null){
+            return;
+        }
+
+        inOrder(node.left, k);
+
+        counter++;
+
+        if(counter == k ){
+            answer = node.val;
+
+            return;
+        }
+        inOrder(node.right,k);
+    }
+}
+
