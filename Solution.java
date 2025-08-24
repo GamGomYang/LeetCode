628494e6756ffd83d628821c5725513a000642c3
GamGomYang <ay6656@naver.com>
2025-08-24 15:55:11 +0900

    08.24 LTC67 Solved

--- Solution.java
+++ Solution.java
@@ -1,35 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return bstChecker(root, null , null);
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0'; // '0' 또는 '1'을 int로 변환
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);   // 현재 자릿수 값
+            carry = sum / 2;      // 올림
         }
 
-    public boolean bstChecker(TreeNode node , Integer min , Integer max){
+        if (carry != 0) sb.append(carry); // 마지막에 올림이 남아있으면 추가
 
-        if(node == null){
-            return true;
-        }
-
-        if((min != null && node.val <= min) || (max != null && node.val >= max)){
-            return false;
-        }
-
-
-        return bstChecker(node.left , min , node.val)&& bstChecker(node.right , node.val , max);
-        
+        return sb.reverse().toString(); // 뒤집어서 반환
     }
 }
+
