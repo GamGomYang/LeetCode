682963051b30d253cd957e6c2e68314746802a22
GamGomYang <ay6656@naver.com>
2025-09-13 18:02:27 +0900

    0913 복습

--- Binary/leetcode67.java
+++ Binary/leetcode67.java
@@ -1,37 +1,39 @@
+import java.util.*;
 
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder binaryBuilder = new StringBuilder();
-
-
         int a_length = a.length()-1;
         int b_length = b.length()-1;
 
+        StringBuilder sb = new StringBuilder();
         int carry =0;
-        int sum =0;
+        
+        while(a_length>=0 || b_length >= 0 || carry >0){
+            int sum =0;
+            int a_size=0;
+            int b_size=0;
 
+            if(a_length >=0){
+            a_size = a.charAt(a_length--)-'0';}
 
-        while(a_length>=0 || b_length>=0 || carry>0 ){
-            sum = carry;
+            if(b_length >= 0){
+            b_size = b.charAt(b_length--)-'0';}
 
-            if(a_length>= 0 ){
-                sum+= a.charAt(a_length--)-'0';
+            sum = a_size+b_size+carry;
+
+            if(sum%2 == 1){
+                sb.append('1');
+            }else{
+                sb.append('0');
             }
-            if(b_length>= 0){
-                sum+= b.charAt(b_length--)-'0';     
-            }
 
-            binaryBuilder.append(sum%2);
+            carry =sum/2;
 
-            carry = sum/2;
         }
 
-        binaryBuilder.reverse();
+        String answer = sb.reverse().toString();
+
+        return answer;
         
-        return binaryBuilder.toString();
-    
-    
-    
     }
 }
-
--- Solution.java
+++ Solution.java
@@ -1,55 +1,39 @@
 import java.util.*;
 
-class MyQueue {
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
+class Solution {
+    public String addBinary(String a, String b) {
+        int a_length = a.length()-1;
+        int b_length = b.length()-1;
 
-    public MyQueue() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int carry =0;
+        
+        while(a_length>=0 || b_length >= 0 || carry >0){
+            int sum =0;
+            int a_size=0;
+            int b_size=0;
 
-    }
+            if(a_length >=0){
+            a_size = a.charAt(a_length--)-'0';}
 
-    public void stack_out(){
-        if(stack2.isEmpty()){
-            while(!stack1.isEmpty()){
-                stack2.push(stack1.pop());
+            if(b_length >= 0){
+            b_size = b.charAt(b_length--)-'0';}
+
+            sum = a_size+b_size+carry;
+
+            if(sum%2 == 1){
+                sb.append('1');
+            }else{
+                sb.append('0');
             }
+
+            carry =sum/2;
+
         }
 
-    }
-    public void push(int x) {
-        stack1.push(x);
-    }
-    
+        String answer = sb.reverse().toString();
 
-    public int pop() {
-        stack_out();
-        int answer =stack2.pop();
         return answer;
-
+        
     }
-    
-
-    public int peek() {
-        stack_out();
-        int answer = stack2.pop();
-        stack2.push(answer);
-        return answer;
-
-    }
-    
-
-    public boolean empty() {
-        if(stack1.isEmpty()&&stack2.isEmpty()){
-
-            return true;
-        }
-        else{
-            return false;
-        }
-
-    }
-
-
 }
