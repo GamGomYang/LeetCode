e83d4cd8e7f7d70c63074da0518d08c012221e6c
GamGomYang <ay6656@naver.com>
2025-09-21 16:53:22 +0900

    LTC937 solving

--- Solution.java
+++ Solution.java
@@ -1,38 +1,21 @@
-//LTC735 solving..
-
 import java.util.*;
 
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        Deque<Integer> stack = new ArrayDeque<>();
+    public int[][] kClosest(int[][] points, int k) {
         
-        int length = asteroids.length;
-        // 자바에서 절댓값 매서드 -> Math.abs
-        for(int i =0 ; i< length ; i++){
-            // int result = 0;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+            (a, b) -> (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1])
+        );
 
-            // if(asteroids[i]>0){
-            //     stack.push(asteroids[i]);
-            // }else{
-            //     int compare_1 = stack.pop();
-            //     int compare_2 = Math.abs(asteroids[i]);
-
-            //     if(compare_1 - compare_2 ==0){
-            //         continue;
-            //     }
-            //     else if(compare2 > compare_1){
-
-
-            //     }
-            // }
-            
+        for (int[] point : points) {
+            minHeap.add(point);
         }
 
-        int[] answer = new int[stack.size()];
-        for (int i = stack.size() - 1; i >= 0; i--) {
-            answer[i] = stack.pop();
+        int[][] answer = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            answer[i] = minHeap.poll();
         }
+
         return answer;
-
     }
 }
