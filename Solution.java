d1616bc4b785346d486e54e5ff14204be98b8e9d
GamGomYang <ay6656@naver.com>
2025-12-18 16:51:49 +0900

    12.18 LTC215 solved

--- Solution.java
+++ Solution.java
@@ -1,12 +1,5 @@
-import java.util.*;
-
-public class Solution{
-    
-    public static void main(String[] args) {
-
-        double compare = 0.6 ;
-
-        System.out.println((int)compare);
-
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
     }
 }
