5c9bba1ac3aada4bbae60fbbbe97aba1bd17c2fd
GamGomYang <ay6656@naver.com>
2026-01-17 21:27:40 +0900

    01.17 LTC310 solving

--- Solution.py
+++ Solution.py
@@ -1,35 +1,33 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        directions = [(1,0) , (0,1), (-1,0) , (0,-1)]
-        def dfs(r, c , idx):
+    def reverseKGroup(self, head, k):
+        if not head or k == 1:
+            return head
 
-            if idx == len(word):
-                return True
-            
-            if r<0 or c<0 or r>=len(board) or c >=len(board[0]):
-                return False
-            if board[r][c] != word[idx]:
-                return False
-            
-            temp = board[r][c]
-            board[r][c] = '#'
-            
-            for dx, dy in directions:
-                if dfs(r+dx , c+dy, idx +1):
-                    return True
-            
-            board[r][c]= temp
-            return False
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        while True:
+            tail = prev
+            for _ in range(k):
+                tail = tail.next
+                if not tail:
+                    return dummy.next
+
+            nxt_group = tail.next
 
 
-        row = len(board)
-        col = len(board[0])
+            curr = prev.next
+            tail.next = None
+            p = None
 
-        for i in range(row):
-            for j in range(col):
+            while curr:
+                nxt = curr.next
+                curr.next = p
+                p = curr
+                curr = nxt
 
-                if board[i][j] == word[0]:
-                    if dfs(i,j, 0):
-                        return True
-                    
-        return False
+            old_start = prev.next
+            prev.next = p
+            old_start.next = nxt_group
+            prev = old_start
