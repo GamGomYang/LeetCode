2e47af5ae7d4a38eb392b58d80e30e921526bbf2
GamGomYang <ay6656@naver.com>
2026-02-06 17:19:28 +0900

    02.06 LTC3379 solved

--- Solution.sql
+++ Solution.sql
@@ -4,3 +4,8 @@ SELECT
     a.city,
     a.state
 FROM Person p LEFT JOIN Address A on p.PersonId = a.PersonId
+
+
+DELETE p1 
+FROM Person p1 JOIN Person p2 ON p1.email = p2.email AND p1.id > p2.id
+
