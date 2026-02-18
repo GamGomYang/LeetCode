6f0b396fd92fbc56331aa4f0dd051ee0e9f420d4
GamGomYang <ay6656@naver.com>
2026-02-18 17:11:58 +0900

    02.18 LTC 96 solved

--- DynamicProgramming/leetcode96.py
+++ DynamicProgramming/leetcode96.py
@@ -1,15 +1,15 @@
 class Solution:
     def numTrees(self, n: int) -> int:
 
-        
         dp = [0]*(n+1)
 
-        dp[0] = 1
-        dp[1] = 1
+        # empty tree is considered a valid bst
+        dp[0] = 1 
 
-        
-        for nodes in range(2,n+1):
-            for root in range(1, nodes +1):
-                dp[nodes] += dp[root - 1]*dp[nodes - root]
+        dp[1] =1
 
-        return dp[n]        
+        for i in range(2,n+1):
+            for j in range(1, i+1):
+                dp[i] += dp[j-1] * dp[i-j]
+
+        return dp[-1]
