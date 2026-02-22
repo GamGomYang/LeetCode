fde4b35c9f9158e02f626f462904eea4fe52b877
GamGomYang <ay6656@naver.com>
2026-02-22 17:23:26 +0900

    02.22 LTC621 soved

--- Solution.py
+++ Solution.py
@@ -1,17 +1,27 @@
+import heapq
+from collections import Counter, deque
+
 class Solution:
-    def countBinarySubstrings(self, s: str) -> int:
-        prev = 0
-        curr = 1
-        result = 0
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        time = 0
+        count = Counter(tasks)
+        #Counter - 딕셔너리 타임 {'a:3'} 형태로 저장되어짐
+
+        max_heap = [-c for c in count.values()]
+
+        heapq.heapify(max_heap)
+
+        cooldown = deque()
+        while max_heap or cooldown:
+            time +=1 
+
+            if max_heap :
+                current = heapq.heappop(max_heap)+1
+                if current != 0 :
+                    cooldown.append((current , time+n))
+            
+            if cooldown and cooldown[0][1] == time:
+                heapq.heappush(max_heap , cooldown.popleft()[0])
+        return time
 
 
-        for i in range(1,len(s)):
-            if s[i] == s[i-1]:
-                curr +=1
-
-            else:
-                result+= min(curr, prev)
-                prev = curr
-                curr = 1
-        result += min(prev , curr)
-        return result
