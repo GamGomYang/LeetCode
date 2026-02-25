fec2e848b778b84693bcb8abe5b05e6e24b77121
GamGomYang <ay6656@naver.com>
2026-02-25 15:37:13 +0900

    02.25 LTC23 solved

--- Heap/leetcode23.py
+++ Heap/leetcode23.py
@@ -8,23 +8,22 @@ import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
-        for i , node  in enumerate(lists):
-            #none 처리
+        for i , node in enumerate(lists):
+            # none일때 인식 안되기 때문에 none처리 해야한다.
             if node:
-                heapq.heappush(heap,(node.val , i , node))
+                heapq.heappush(heap, (node.val , i , node))
 
         dummy = ListNode(0)
         current = dummy
 
-
-        while heap:
+        while heap: 
             val , i , node = heapq.heappop(heap)
-
             current.next = node 
             current = current.next
 
             if node.next:
-                heapq.heappush(heap , (node.next.val, i , node.next))
-
+                heapq.heappush(heap , (node.next.val , i , node.next))
 
         return dummy.next
+
+
