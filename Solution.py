2464630e67ae7d793254d8f8d5a0b0265011acee
GamGomYang <ay6656@naver.com>
2026-04-01 00:35:03 +0900

    04.01 modified

--- Solution.py
+++ Solution.py
@@ -1,30 +1,25 @@
-import heapq
-from collections import Counter , deque
+from collections import defaultdict
 
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
 
-        # Count - 자동 카운팅 딕셔너리
-        # a,a,b,b,b -> a:2 , b:3 이렇게 자동 딕셔너리로 나옴
-        count = Counter(tasks)
-        max_heap = [-c for c in count.values()]
-        # 리스트를 한번에 heap으로 만들기
-        # for문을 이용한 heappush보다 더 빠르다.
-        heapq.heapify(max_heap)
+        ransom_result = defaultdict(int)
+        magazine_result = defaultdict(int)
+
+        for a in ransomNote :
+            ransom_result[a] += 1
+
+        for b in magazine:
+            magazine_result[b] +=1 
+
+
+        for c in ransom_result:
+            if magazine_result[c] != ransom_result[c] :
+                return False
+
+
+        return True
+
+
         
-        waiting = deque()
-        time = 0
-
-        while max_heap or waiting:
-            time += 1
-            
-            if max_heap :
-                current = heapq.heappop(max_heap)+1
-                if current != 0:
-                    waiting.append((current, time + n))
-                
-            if waiting and waiting[0][1] == time:
-                heapq.heappush(max_heap, waiting.popleft()[0])
-
-        return time
