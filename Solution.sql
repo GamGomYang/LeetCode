92eea77a940a3e1d95dad1c7c35e9ea8a7b7bf2f
GamGomYang <ay6656@naver.com>
2026-06-19 17:48:48 +0900

    06.19 LTC585 solved

--- Solution.sql
+++ Solution.sql
@@ -141,3 +141,19 @@ SELECT name
 FROM Customer
 WHERE referee_id IS NULL 
 OR referee_id <> 2
+
+SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+  SELECT tiv_2015
+  FROM Insurance
+  GROUP BY tiv_2015
+  HAVING COUNT(*)>1
+)
+AND
+(lat , lon) IN (
+  SELECT lat , lon
+  FROM Insurance 
+  GROUP BY lat , lon
+  HAVING COUNT(*) = 1
+)
