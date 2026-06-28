ed025116b80ae2cc22156b4eb8958823159fea79
GamGomYang <ay6656@naver.com>
2026-06-28 16:45:52 +0900

    06.28 insert interval solved

--- Solution.py
+++ Solution.py
@@ -1,30 +1,24 @@
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         nums_map = {}
-
-#         for idx , num in enumerate(nums):
-#             need = target - num
-
-#             if need in nums_map:
-#                 return [nums_map[need] , idx]
-            
-#             nums_map[num] = idx
-                
-
-
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-        
-#         min_price = prices[0]
-#         max_profit = 0
-
-#         for i in prices:
-#             min_price = min(min_price , i)
-#             max_profit = max(max_profit , i - min_price)
-
-#         return max_profit 
-
-
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
+        result = []
+
+        for interval in intervals:
+            if interval[0]>newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            
+            elif interval[1]<newInterval[0]:
+                result.append(interval)
+
+            else:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+
+        result.append(newInterval)
+
+        return result
+
+                
+
+            
