d5ed1e38aadb6a25facbe88f302de1462af95642
GamGomYang <ay6656@naver.com>
2026-06-30 17:42:13 +0900

    06.30 LTC 610 solved

--- Solution.sql
+++ Solution.sql
@@ -276,3 +276,15 @@ SELECT id,
           ELSE 'Leaf' 
         END AS type
 FROM tree
+
+
+SELECT x , y , z , 
+      CASE 
+        WHEN 
+        x+y > z AND 
+        y+z > x AND 
+        z+x > y
+        THEN 'Yes'
+        ELSE 'No'
+        END AS triangle
+FROM Triangle
