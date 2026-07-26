0a31d93defe8185d7655e6f38852e52f4f9827f1
GamGomYang <ay6656@naver.com>
2026-07-26 18:44:45 +0900

    LTC2273 solved

--- Array/leetcode1.py
+++ Array/leetcode1.py
@@ -1,3 +1,15 @@
 class Solution:
-    def twoSum(self, nums: List[int],target: int)->List[int]:
-        
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hash = {}
+
+        for i, num in enumerate(nums):
+
+            needed = target - num
+
+            if needed in hash:
+                return [hash[needed] , i]
+
+            hash[num] = i 
+
+        return []
