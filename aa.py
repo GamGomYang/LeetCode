8d189a08df3354408830866219623511211009bd
GamGomYang <ay6656@naver.com>
2026-08-28 17:03:41 +0900

    LTC739 solved

--- aa.py
+++ aa.py
@@ -1,16 +1,41 @@
+from collections import deque 
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
-        intervals.sort(key = lambda x : x[0])
+        width = len(grid[0])
+        height = len(grid)
 
-        result = []
+        queue = deque()
+        area = 0
+        max_area = 0
 
-        for interval in intervals:
-            if not result or result[-1][1] < interval[0]:
-                result.append(interval)
+        directions = [(-1,0),(0,-1),(1,0),(0,1)]
 
-            else:
-                result[-1][1] = max(result[-1][1], interval[1]) 
+        for i in range(height):
+            for j in range(width):
+
+                if grid[i][j] == 1 :
+                    queue.append((i,j))
+                    area =1
+                    grid[i][j] = 0
+
+                    while queue :
+                        row, col = queue.popleft()
+
+                        for dr , dc in directions:
+                            nr = row + dr
+                            nc = col + dc
+
+                            if 0<= nr < height and \
+                                0<= nc < width and \
+                                grid[nr][nc] == 1:
+                                    area +=1
+                                    grid[nr][nc] = 0
+                                    queue.append((nr,nc))
+
+                        max_area = max(max_area , area)
+
+        return max_area
 
 
-        return result         
+
