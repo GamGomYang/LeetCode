f7937b93161de78c568ee8fc21d902559e8e67e9
GamGomYang <ay6656@naver.com>
2026-08-29 20:58:50 +0900

    LTC1174 solved

--- Solution.sql
+++ Solution.sql
@@ -319,3 +319,29 @@ SELECT project_id , ROUND(AVG(experience_years),2) AS average_years
 FROM Project p  JOIN Employee e
 ON p.employee_id = e.employee_id
 GROUP BY project_id 
+
+
+SELECT 
+ROUND (
+  AVG(
+    CASE 
+      WHEN d.order_date = d.customer_pref_delivery_date 
+      THEN 1 
+      ELSE 0
+    END
+  )*100,2 
+) AS immediate_percentage
+FROM Delivery d 
+JOIN (
+  SELECT customer_id , MIN(order_date) 
+  AS first_order_date
+  FROM Delivery
+  GROUP BY customer_id 
+) f
+ON d.customer_id = f.customer_id 
+AND d.order_date = f.first_order_date
+
+ 
+
+
+
